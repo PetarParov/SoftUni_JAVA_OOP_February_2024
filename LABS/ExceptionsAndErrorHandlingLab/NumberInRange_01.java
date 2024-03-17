e6f437f5ab22f5765bc2969438cd58787267361b
Petar Parov <petarparov@gmail.com>
2024-03-17 19:02:28 +0200

    Added solutions to Exception and Error Handling Lab

---
+++ LABS/ExceptionsAndErrorHandlingLab/NumberInRange_01.java
@@ -0,0 +1,31 @@
+package ExceptionsAndErrorHandlingLab;
+
+import java.util.Scanner;
+
+public class NumberInRange_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] range = scanner.nextLine().split("\\s+");
+        int startRange = Integer.parseInt(range[0]);
+        int endRange = Integer.parseInt(range[1]);
+        System.out.printf("Range: [%d...%d]%n", startRange, endRange);
+        int validNumber = readValidNumber(scanner, startRange, endRange);
+        System.out.printf("Valid number: %d%n", validNumber);
+    }
+
+    private static int readValidNumber(Scanner scanner, int startRange, int endRange) {
+        while (true) {
+            String input = scanner.nextLine();
+            try {
+                int number = Integer.parseInt(input);
+                if (number >= startRange && number <= endRange) {
+                    return number;
+                }
+            } catch (NumberFormatException exception) {
+
+            }
+
+            System.out.printf("Invalid number: %s%n", input);
+        }
+    }
+}
